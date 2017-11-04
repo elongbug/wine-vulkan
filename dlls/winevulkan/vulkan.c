838d2f770c988950b900940fb56836bf7ede03a4
Roderick Colenbrander <thunderbird2k@gmail.com>
2017-11-04 16:26:23 -0700

    winevulkan: Define vulkan driver interface.
    Signed-off-by: Roderick Colenbrander <thunderbird2k@gmail.com>

--- dlls/winevulkan/vulkan.c
+++ dlls/winevulkan/vulkan.c
@@ -21,9 +21,11 @@
 
 #include "windef.h"
 #include "winbase.h"
+#include "winuser.h"
 
 #include "wine/debug.h"
 #include "wine/vulkan.h"
+#include "wine/vulkan_driver.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(vulkan);
 
@@ -45,6 +47,26 @@ struct vulkan_func
 };
 
 static void *wine_vk_get_global_proc_addr(const char *name);
+
+static const struct vulkan_funcs *vk_funcs = NULL;
+
+static BOOL wine_vk_init(HINSTANCE hinst)
+{
+    HDC hdc = GetDC(0);
+
+    vk_funcs =  __wine_get_vulkan_driver(hdc, WINE_VULKAN_DRIVER_VERSION);
+    if (!vk_funcs)
+    {
+        ERR("Failed to load Wine graphics driver supporting Vulkan.\n");
+        ReleaseDC(0, hdc);
+        return FALSE;
+    }
+
+    DisableThreadLibraryCalls(hinst);
+
+    ReleaseDC(0, hdc);
+    return TRUE;
+}
 
 static VkResult WINAPI wine_vkCreateInstance(const VkInstanceCreateInfo *pCreateInfo, const VkAllocationCallbacks *pAllocator,
         VkInstance *pInstance)
@@ -121,8 +143,7 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
     switch(reason)
     {
         case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls(hinst);
-            break;
+            return wine_vk_init(hinst);
 
         case DLL_THREAD_ATTACH:
             break;
